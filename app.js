98093274c8b7365e7ff6a67f7d782a07d4666a2f
Yuri Chetverikov <bashered@gmail.com>
2022-07-29 17:01:56 +0300

    add: Сделать схемы. Добавить обработчики ошибок. Подготовить файл app.js к настройке роутов

---
+++ app.js
@@ -0,0 +1,40 @@
+require('dotenv').config();
+const helmet = require('helmet');
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const { errors } = require('celebrate');
+const NotFoundError = require('./errors/NotFoundError');
+
+const { PORT = 3001 } = process.env;
+const app = express();
+app.use(helmet());
+
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+mongoose.connect('mongodb://localhost:27017/moviesdb', {
+  useNewUrlParser: true,
+});
+
+app.use((req, res, next) => {
+  next(new NotFoundError('Путь не найден'));
+});
+
+app.use(errors());
+
+// eslint-disable-next-line no-unused-vars
+app.use((err, req, res, next) => {
+  const { statusCode = 500, message } = err;
+
+  res
+    .status(statusCode)
+    .send({
+      message: statusCode === 500
+        ? 'На сервере произошла ошибка'
+        : message,
+    });
+});
+
+app.listen(PORT, () => {});
